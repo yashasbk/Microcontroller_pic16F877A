f18bb1a57105d8b6a7b4472e9d3d4afcdeaa8a71
Yashas B K <38299135+yashasbk@users.noreply.github.com>
2022-03-16 12:04:12 +0530

    Update Timer_initial_value_calculator.c

--- Timer_initial_value_calculator.c
+++ Timer_initial_value_calculator.c
@@ -5,22 +5,21 @@ Description : This code gives the initial value for the timer register
 
 #define INITIAL 1  //This is the initial value Note:never make it 0
 #define MAX     256  // This the resolution of the timer 2^8 or 2^16
+#define TIME    1000000000  //Enter time you need in nano seconds
+#define QUANTUM 200  //Enter the time required for tick in your uC
 #include <stdio.h>
 
 int main()
 {
-    int count=0,quantum=200;
-    long time = 1000000000;
     for(int i=INITIAL; i<= MAX; i++)
     {
-        if(time%(quantum*i)==0)
+        if(TIME%(QUANTUM*i)==0)
         {
             printf("\ni is %d\n",i);
-            printf("Count value is %ld",time/(quantum*i));
+            printf("Count value is %ld",TIME/(QUANTUM*i));
             printf("\nInitial value to set in timer is %d",MAX - i);
         }
     }
-    
     
     return 0;
 }
